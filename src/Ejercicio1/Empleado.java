701c2845ad544b88570d4ecafc699490415f0b0d
mrmalvicino <mrmalvicino@gmail.com>
2024-08-12 23:27:24 -0300

    Encapsulación

--- src/Ejercicio1/Empleado.java
+++ src/Ejercicio1/Empleado.java
@@ -2,11 +2,41 @@ package Ejercicio1;
 
 public class Empleado {
 	
-	//atributos
+	// Atributos
 	
-	int id; 
-	String nombre;
-	int edad; 
+	private int id; 
+	private String nombre;
+	private int edad;
 	
-
+	// Getters y setters
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public String getNombre()
+	{
+		return nombre;
+	}
+	
+	public void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+	
+	public int getEdad()
+	{
+		return edad;
+	}
+	
+	public void setEdad(int edad)
+	{
+		this.edad = edad;
+	} 
 }
